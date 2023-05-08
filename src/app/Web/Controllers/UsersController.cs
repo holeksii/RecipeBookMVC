4a5edc60194104aeeb79fb2d37d855fc984420cd
ULiesnik <uljana042003@gmail.com>
2023-05-08 20:09:48 +0300

    own account page added

--- src/app/Web/Controllers/UsersController.cs
+++ src/app/Web/Controllers/UsersController.cs
@@ -6,15 +6,24 @@ using Business.Services;
 public class UsersController : Controller
 {
     private readonly IUserService _userService;
+    private readonly IContextService _contextService;
 
-    public UsersController(IUserService userService)
+    public UsersController(IUserService userService, IContextService contextService)
     {
         _userService = userService;
+        _contextService = contextService;
     }
 
     [HttpGet("{id}")]
     public ActionResult UserInfo(string id)
     {
         return View("UserInfo", _userService.GetUser(id));
+    }
+
+    [HttpGet("MyAccount")]
+    public ActionResult MyAccount()
+    {
+        string _currentId = _contextService.GetUserId();
+        return View("UserInfo", _userService.GetUser(_currentId));
     }
 }
